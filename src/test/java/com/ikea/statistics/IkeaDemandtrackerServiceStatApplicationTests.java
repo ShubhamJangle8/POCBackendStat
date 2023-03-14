510c1f0dcd15f67ff802a672e7d3afb5cb4903c5
Shubham Jangale <shubham-rajendra.jangale@capgemini.com>
2023-03-14 12:42:47 +0530

    commented test code

--- src/test/java/com/ikea/statistics/IkeaDemandtrackerServiceStatApplicationTests.java
+++ src/test/java/com/ikea/statistics/IkeaDemandtrackerServiceStatApplicationTests.java
@@ -3,11 +3,11 @@ package com.ikea.statistics;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class IkeaDemandtrackerServiceStatApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class IkeaDemandtrackerServiceStatApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
